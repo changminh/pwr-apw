db207eff6207ddf9aae9013baeea4ca343d3fad3
Greg.Matoga <Greg.Matoga@d15b99e0-13d8-11de-b69a-45dc1c5e4070>
2009-07-27 14:06:59 +0000

    Small updates

---
+++ APW/src/apw/gui/par/ParameterClassGUIBuilder.java
@@ -0,0 +1,83 @@
+/*
+ *  Copyright (c) 2009, Wrocław University of Technology
+ *  All rights reserved.
+ *  Redistribution  and use in source  and binary  forms,  with or
+ *  without modification,  are permitted provided that the follow-
+ *  ing conditions are met:
+ * 
+ *   • Redistributions of source code  must retain the above copy-
+ *     right  notice, this list  of conditions and  the  following
+ *     disclaimer.
+ *   • Redistributions  in binary  form must  reproduce the  above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer  in  the  documentation and / or other materials
+ *     provided with the distribution.
+ *   • Neither  the name of the  Wrocław University of  Technology
+ *     nor the names of its contributors may be used to endorse or
+ *     promote products derived from this  software without speci-
+ *     fic prior  written permission.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRI-
+ *  BUTORS "AS IS" AND ANY  EXPRESS OR IMPLIED WARRANTIES, INCLUD-
+ *  ING, BUT NOT  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTA-
+ *  BILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+ *  NO EVENT SHALL THE COPYRIGHT HOLDER OR  CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT,  INCIDENTAL, SPECIAL,  EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCURE-
+ *  MENT OF SUBSTITUTE  GOODS OR SERVICES;  LOSS OF USE,  DATA, OR
+ *  PROFITS; OR BUSINESS  INTERRUPTION) HOWEVER  CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ *  TORT (INCLUDING  NEGLIGENCE  OR  OTHERWISE) ARISING IN ANY WAY
+ *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSI-
+ *  BILITY OF SUCH DAMAGE.
+ */
+package apw.gui.par;
+
+import apw.classifiers.knn.KNNProperties;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import net.java.dev.properties.BaseBean;
+import net.java.dev.properties.RProperty;
+import net.java.dev.properties.binding.swing.SwingFactory;
+
+/**
+ *
+ * @author Greg Matoga <greg dot matoga at gmail dot com>
+ */
+public class ParameterClassGUIBuilder extends BaseBean {
+
+    public static JComponent buildView(BaseBean propertiesBean) {
+        JComponent UI = SwingFactory.boxColumnLayoutFactory().createBeanUI(propertiesBean);
+        return UI;
+    }
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            Logger.getLogger(ParameterClassGUIBuilder.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        final KNNProperties props = new KNNProperties();
+        JFrame frame = new JFrame("Bean UI test");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(buildView(props));
+        frame.pack();
+        frame.addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.out.println("Window closing");
+                for (RProperty p : Arrays.asList(props.getRProperties()))
+                    System.out.println("p:" + p + "; object: " + p.get());
+            }
+        });
+        frame.setVisible(true);
+    }
+}
