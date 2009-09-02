fc79ea25db7aeff4f57a4ce691c8b3addf8f8904
kwas.mariusz <kwas.mariusz@d15b99e0-13d8-11de-b69a-45dc1c5e4070>
2009-09-02 15:27:35 +0000

    [Nitric] Nie za bardzo działająca sieć ART2 :/

--- APW/src/apw/art2/Network.java
+++ APW/src/apw/art2/Network.java
@@ -57,12 +57,27 @@ public class Network {
     private static int neuronCounter = 0;
     private double initialBottomUp;
     private double a, b, c, d, rho, theta;
-    private final double e = 0.0001d;
+    private final double e = 0.d;
+    private double doubleTheta, thetaTheta;
     private double[] actualInput;
     private double updateDenominator;
-    private AttentionalSubsystem attentional = new AttentionalSubsystem();
-    private OrientingSubsystem orienting = new OrientingSubsystem();
+    private AttentionalSubsystem attention;
+    private OrientingSubsystem orienting;
     private HashSet<Integer> banned = new HashSet<Integer>();
+
+    // Sublayer indexes:
+    private static final int w = 0;
+    private static final int x = 1;
+    private static final int v = 2;
+    private static final int u = 3;
+    private static final int p = 4;
+    private static final int q = 5;
+
+    // Sublayler outputs: previous iteration and actual iteration
+    private static final int prev = 0;
+    private static final int pres = 1;
+
+    private static final int sublayers = 6;
 
     private static final Random rand = new Random();
 
@@ -75,12 +90,14 @@ public class Network {
         this.rho = rho;
         this.theta = theta;
         try {
-            // ART2_Util.checkParameters(a, b, c, d, rho, theta);
+            ART2_Util.checkParameters(a, b, c, d, rho, theta);
         }
         catch (IllegalArgumentException ex) {
             JOptionPane.showMessageDialog(null, ex.getMessage());
             throw new ExceptionInInitializerError();
         }
+        attention = new AttentionalSubsystem();
+        orienting = new OrientingSubsystem();
         initialize();
     }
 
@@ -92,8 +109,8 @@ public class Network {
         actualInput = input;
         outer:
         while(!resonance) {
-            attentional.processF1();                    // Needs to be repeated
-            attentional.processF1();
+            attention.processF1();                    // Needs to be repeated
+            attention.processF1();
             orienting.process();
             inner:
             while(!resonance) {
@@ -101,7 +118,7 @@ public class Network {
                 if (reset) {
                     // Reset...
                     cycleCounter = 1;
-                    for (Neuron n : attentional.y)
+                    for (Neuron n : attention.f2)
                         if (n.output > 0)
                             banned.add(n.id);
                     continue outer;
@@ -115,7 +132,7 @@ public class Network {
                     double temp;
                     // Competition:
                     winnerIndex = -1;
-                    for (Neuron n : attentional.y) {
+                    for (Neuron n : attention.f2) {
                         temp = n.process();
                         // System.out.println("temp = " + temp);
                         if (temp > maxNet && !banned.contains(n.id)) {
@@ -125,17 +142,17 @@ public class Network {
                         counter++;
                     }
                     // System.out.println("");
-                    for (Neuron n : attentional.y)
+                    for (Neuron n : attention.f2)
                         n.setWinner(winnerIndex);
                     // Updating output of layers:
-                    attentional.updatePQ();
-                    attentional.processF1();
+                    attention.updatePQ();
+                    attention.processF1();
                     continue inner;
                 }
                 else {
                     // Resonance:
                     resonance = true;
-                    attentional.y.get(winnerIndex).updateWeights();
+                    attention.f2.get(winnerIndex).updateWeights();
                 }
             }
         }
@@ -144,14 +161,17 @@ public class Network {
 
     private void initialize() {
         // Calculate bootom-up initial weights limit:
-        initialBottomUp = (1.05d / ((1 - d) * Math.sqrt(dimension)));            // 1.05 - Warning :P
-        for (int i = 0; i < 5; i++)
-            attentional.y.add(new Neuron(neuronCounter++));
+        initialBottomUp = (1.015d / ((1 - d) * Math.sqrt(dimension)));            // 1.05 - Warning :P
+        for (int i = 0; i < 150; i++)
+            attention.f2.add(new Neuron(neuronCounter++));
         updateDenominator = 1.d - d;
+        thetaTheta = theta * theta;
+        doubleTheta = 2 * theta;
     }
 
-    private double f(double param) {
-        return param > theta ? param : 0.d;
+    private double f(double par) {
+        return par > theta ? par : 0.d;
+            // par : (doubleTheta * par * par) / (par * par + thetaTheta);
     }
 
     private boolean vigilanceTest() {
@@ -160,37 +180,52 @@ public class Network {
 
     private void resetOutput() {
         cycleCounter = 1;
-        attentional.clearOutput();
+        attention.clearOutput();
         orienting.clearOutput();
     }
 
     class AttentionalSubsystem {
 
-        private double[] w, x, v, u, p, q;                      // 6 sublayers of F1 layer
-        private ArrayList<Neuron> y = new ArrayList<Neuron>();  // F2 layer
+        private double[][][] f1 = new double[2][sublayers][];    // output of 6 sublayers of F1 layer
+        private ArrayList<Neuron> f2 = new ArrayList<Neuron>();  // F2 layer
 
         public AttentionalSubsystem() {
+            for (int i = 0; i < sublayers; i++)
+                f1[1][i] = new double[dimension];
             clearOutput();
         }
 
-        public void processF1() {
-            processW();
-            processX();
-            processV();
-            processU();
-            processP();
-            processQ();
+        private void processF1() {
+            int counter = 0;
+            boolean stabilization = false;
+            while (!stabilization) {
+                counter++;
+                stabilization = true;
+                for (int i = 0; i < sublayers; i++)
+                    for (int j = 0; j < dimension; j++)
+                        f1[prev][i][j] = f1[pres][i][j];
+                processW();
+                processX();
+                processV();
+                processU();
+                processP();
+                processQ();
+                // Check whether there were changes of weights:
+                for (int i = 0; i < sublayers; i++)
+                    for (int j = 0; j < dimension; j++)
+                        if (Math.abs(f1[prev][i][j] - f1[pres][i][j]) > 0.0001)
+                            stabilization = false;
+            }
+            System.out.println("counter = " + counter);
         }
 
         private void clearOutput() {
-            w = new double[dimension];
-            x = new double[dimension];
-            v = new double[dimension];
-            u = new double[dimension];
-            p = new double[dimension];
-            q = new double[dimension];
+            for (int i = 0; i < sublayers; i++) {
+                f1[0][i] = f1[1][i];
+                f1[1][i] = new double[dimension];
+            }
             // Reseting output of neurons:
-            for (Neuron n: y)
+            for (Neuron n: f2)
                 n.setWinner(-1);
         }
 
@@ -201,48 +236,48 @@ public class Network {
 
         private void processW() {
             for (int i = 0; i < dimension; i++) {
-                attentional.w[i] = actualInput[i] + a * attentional.u[i];
+                attention.f1[pres][w][i] = actualInput[i] + a * attention.f1[pres][u][i];
             }
         }
 
         private void processX() {
-            double denominator = e + vectorLength(attentional.w);
+            double denominator = e + vectorLength(attention.f1[pres][w]);
             for (int i = 0; i < dimension; i++) {
-                attentional.x[i] = attentional.w[i] / denominator;
+                attention.f1[pres][x][i] = attention.f1[pres][w][i] / denominator;
             }
         }
 
         private void processV() {
             for (int i = 0; i < dimension; i++) {
-                attentional.v[i] = f(attentional.x[i]) + b * f(attentional.q[i]);
+                attention.f1[pres][v][i] = f(attention.f1[pres][x][i]) + b * f(attention.f1[pres][q][i]);
             }
         }
 
         private void processU() {
-            double denominator = e + vectorLength(attentional.v);
+            double denominator = e + vectorLength(attention.f1[pres][v]);
             for (int i = 0; i < dimension; i++)
-                attentional.u[i] = attentional.v[i] / denominator;
+                attention.f1[pres][u][i] = attention.f1[pres][v][i] / denominator;
         }
 
         private void processP() {
             // System.out.print("procesP: ");
             if (winnerIndex != -1) {
                 // System.out.println("F2 active!");
-                Neuron winner = attentional.y.get(winnerIndex);
+                Neuron winner = attention.f2.get(winnerIndex);
                 for (int i = 0; i < dimension; i++)
-                    attentional.p[i] = attentional.u[i] + d * winner.topDown[i];
+                    attention.f1[pres][p][i] = attention.f1[pres][u][i] + d * winner.topDown[i];
             }
             else {
                 // System.out.println("F2 inactive!");
                 for (int i = 0; i < dimension; i++)
-                    attentional.p[i] = attentional.u[i];
+                    attention.f1[pres][p][i] = attention.f1[pres][u][i];
             }
         }
 
         private void processQ() {
-            double denominator = e + vectorLength(attentional.p);
+            double denominator = e + vectorLength(attention.f1[pres][p]);
             for (int i = 0; i < dimension; i++)
-                attentional.q[i] = attentional.p[i] / denominator;
+                attention.f1[pres][q][i] = attention.f1[pres][p][i] / denominator;
         }
     }
 
@@ -256,9 +291,9 @@ public class Network {
 
         private void process() {
             double denominator =
-                    e + vectorLength(attentional.u) + vectorLength(scaleVector(attentional.p, c));
+                    e + vectorLength(attention.f1[pres][u]) + c * vectorLength(attention.f1[pres][p]);
             for (int i = 0; i < dimension; i++)
-                r[i] = (attentional.u[i] + c * attentional.p[i]) / denominator;
+                r[i] = (attention.f1[pres][u][i] + c * attention.f1[pres][p][i]) / denominator;
         }
 
         private void clearOutput() {
@@ -284,7 +319,7 @@ public class Network {
         private double process() {
             net = 0.d;
             for (int i = 0; i < dimension; i++) {
-                net += attentional.p[i] * bottomUp[i];
+                net += attention.f1[pres][p][i] * bottomUp[i];
             }
             return net;
         }
@@ -295,16 +330,20 @@ public class Network {
 
         public void updateWeights() {
             for (int i = 0; i < dimension; i++)
-                bottomUp[i] = topDown[i] = attentional.u[i] / updateDenominator;
+                bottomUp[i] = topDown[i] = attention.f1[pres][u][i] / updateDenominator;
         }
     }
 
     public static void main(String[] args) throws Exception {
-        // Network n = new Network(4, 10, 10, 0.1d, 0.9d, 0.9, 0.2);
+        // Network n = new Network(4, 10, 10, 0.1d, 0.9d, 0.9, 0.01);
         // Samples samples = new ARFFLoader(new File("data/iris.arff")).getSamples();
-        Network n = new Network(4, 10, 10, 0.1d, 0.9d, 0.99, 0.01);
-        Samples samples = new ARFFLoader(new File("data/iris.arff")).getSamples();
+        Network n = new Network(4, 10, 10, 0.1d, 0.9d, 0.999d, 0.01d);
+        Samples samples = new ARFFLoader(new File("data/test.arff")).getSamples();
         ArrayList<double[]> inst = new ArrayList<double[]>();
+        // n.process(new double[] {0.2, 0.7, 0.1, 0.5, 0.4});
+        if (false)
+            return;
+        int[] results = new int[30];
         double[] temp;
         for (Sample s : samples) {
             temp = new double[4];
@@ -316,6 +355,9 @@ public class Network {
             for (int j = 0; j < inst.size(); j++)
                 n.process(inst.get(j));
         for (int j = 0; j < inst.size(); j++)
-            System.out.println(j + " = " + n.process(inst.get(j)));
+            results[n.process(inst.get(j))]++;
+        for (int i = 0; i < results.length; i++)
+            System.out.print(results[i] + " ");
+        System.out.println("");
     }
 }
